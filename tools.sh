c7fd99273183a90f4219f2c4124b23c959ca071d
LANIAKEA <74137716+ZCXYHQ@users.noreply.github.com>
2023-11-01 09:13:35 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -89,67 +89,6 @@ sed -i "s/net.ipv4.icmp_echo_ignore_broadcasts=1/net.ipv4.icmp_echo_ignore_broad
 sysctl -p && sysctl --system
 }
 
-ulimit_tune(){
-
-echo "1000000" > /proc/sys/fs/file-max
-sed -i '/fs.file-max/d' /etc/sysctl.conf
-cat >> '/etc/sysctl.conf' << EOF
-fs.file-max=1000000
-EOF
-
-ulimit -SHn 1000000 && ulimit -c unlimited
-echo "root     soft   nofile    1000000
-root     hard   nofile    1000000
-root     soft   nproc     1000000
-root     hard   nproc     1000000
-root     soft   core      1000000
-root     hard   core      1000000
-root     hard   memlock   unlimited
-root     soft   memlock   unlimited
-
-*     soft   nofile    1000000
-*     hard   nofile    1000000
-*     soft   nproc     1000000
-*     hard   nproc     1000000
-*     soft   core      1000000
-*     hard   core      1000000
-*     hard   memlock   unlimited
-*     soft   memlock   unlimited
-">/etc/security/limits.conf
-if grep -q "ulimit" /etc/profile; then
-  :
-else
-  sed -i '/ulimit -SHn/d' /etc/profile
-  echo "ulimit -SHn 1000000" >>/etc/profile
-fi
-if grep -q "pam_limits.so" /etc/pam.d/common-session; then
-  :
-else
-  sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
-  echo "session required pam_limits.so" >>/etc/pam.d/common-session
-fi
-
-sed -i '/DefaultTimeoutStartSec/d' /etc/systemd/system.conf
-sed -i '/DefaultTimeoutStopSec/d' /etc/systemd/system.conf
-sed -i '/DefaultRestartSec/d' /etc/systemd/system.conf
-sed -i '/DefaultLimitCORE/d' /etc/systemd/system.conf
-sed -i '/DefaultLimitNOFILE/d' /etc/systemd/system.conf
-sed -i '/DefaultLimitNPROC/d' /etc/systemd/system.conf
-
-cat >>'/etc/systemd/system.conf' <<EOF
-[Manager]
-#DefaultTimeoutStartSec=90s
-DefaultTimeoutStopSec=30s
-#DefaultRestartSec=100ms
-DefaultLimitCORE=infinity
-DefaultLimitNOFILE=65535
-DefaultLimitNPROC=65535
-EOF
-
-systemctl daemon-reload
-
-}
-
 bbr(){
 
 if uname -r|grep -q "^6."
@@ -248,11 +187,10 @@ get_system_info() {
 menu() {
   echo -e "\
 ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
-${Green_font_prefix}1.${Font_color_suffix} 安装BBR原版内核(已经是5.x的不需要)
+${Green_font_prefix}1.${Font_color_suffix} 安装BBR原版内核(已经是6.x的不需要)
 ${Green_font_prefix}2.${Font_color_suffix} TCP窗口调优
 ${Green_font_prefix}3.${Font_color_suffix} 开启内核转发
-${Green_font_prefix}4.${Font_color_suffix} 系统资源限制调优
-${Green_font_prefix}5.${Font_color_suffix} 屏蔽ICMP ${Green_font_prefix}6.${Font_color_suffix} 开放ICMP
+${Green_font_prefix}4.${Font_color_suffix} 屏蔽ICMP ${Green_font_prefix}5.${Font_color_suffix} 开放ICMP
 "
 get_system_info
 echo -e "当前系统信息: ${Font_color_suffix}$opsy ${Green_font_prefix}$virtual${Font_color_suffix} $arch ${Green_font_prefix}$kern${Font_color_suffix}
@@ -273,12 +211,9 @@ echo -e "当前系统信息: ${Font_color_suffix}$opsy ${Green_font_prefix}$virt
     enable_forwarding
     ;;
   4)
-    ulimit_tune
-    ;;
-  5)
     banping
     ;;
-  6)
+  5)
     unbanping
     ;;
   *)
